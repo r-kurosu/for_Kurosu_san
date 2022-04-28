0f1cd2d0cc0786a45021f5e091e1306d2a0b7afa
ryo_kurosu <dtrjs.kurosu.0510@gmail.com>
2022-04-28 18:08:31 +0900

    アルゴリズム大枠だけ

---
+++ dt_algorithm.py
@@ -0,0 +1,55 @@
+import numpy as np
+import pandas as pd
+import pulp
+import time, sys, copy, itertools, math, warnings, os
+
+from sklearn.model_selection import KFold
+from sklearn.linear_model import Lasso
+from sklearn.feature_selection import SelectKBest, f_regression
+
+warnings.simplefilter('ignore')
+
+
+def read_data():
+
+    return
+
+
+def grouping_func():
+
+    return
+
+
+def execute_LP():
+
+    return
+
+
+def sort_data():
+
+    return
+
+
+def search_merge():
+
+    return
+
+
+def redefine_func():
+
+    return
+
+
+def main():
+    read_data()
+    grouping_func()
+    execute_LP()
+    sort_data()
+    search_merge()
+    redefine_func()
+
+    return 0
+
+
+if __name__ == "__main__":
+    main()
